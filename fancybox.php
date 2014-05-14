703563ebce3e4f1c72b632c4fef0b6e98c6819e1
Rafael Neri <rafepel@gmail.com>
2014-05-14 13:12:13 -0300

    Initial Commit - Beta Release
    Signed-off-by: Rafael Neri <rafepel@gmail.com>

---
+++ fancybox.php
@@ -0,0 +1,84 @@
+<div class="fmsg" id="downloads">
+    <h4 align="center">Escolha uma das opções de download:</h4>
+    <div style="text-align: center">
+        <a href="<?php echo $url_iso32; ?>" class="btn btn-large btn-info">32 Bits</a>
+        <a href="<?php echo $url_iso64; ?>" class="btn btn-large btn-info">64 Bits</a>
+    </div>
+    <div>
+        <strong>Observações:</strong>
+        <ul>
+            <li>32 bits com PAE ativo, o que permite acesso até 64 GB de memória RAM.</li>
+            <li>64 bits com suporte a EFI.</li>
+            <li>Caso o computador tenha menos de 6 GB de RAM é recomendado utilizar a ISO de 32 bits.</li>
+        </ul>
+    </div>
+    <div>
+        <strong>MD5 32-Bits:</strong> 147cad7c17b9fadbfff7e0a3518f56a7
+        <br/>
+        <strong>MD5 64-Bits:</strong> b1a82019a69de8e0eaedc84b77a516e9
+    </div>
+</div>
+        
+
+
+<div class="fmsg" id="notas_versao">
+    <h4>Notas da Versão</h4>
+    <p>
+        Essa versão é construída utilizando o Kaiana Iso Generator (KIG) 1.0.6, uma ferramenta de geração de sistemas a partir dos pacotes compilados de um repositório. No caso utilizamos o Repositório Ubuntu, alguns repositórios extras e o repositório do Projeto União Livre. Os pacotes da base são do Ubuntu 14.04 e Kernel 3.13.0-11-generic. O Sistema Kaiana também vem com uma seleção de aplicativos para o seu uso diário. Procuramos criar uma seleção que ofereça comodidade e facilidade. Veja a lista abaixo.
+    </p>
+    <p>
+        <strong>Escritório:</strong>
+        <ul>
+            <li>LibreOffice</li>
+            <li>Okular</li>
+        </ul>
+        <strong>Internet:</strong>
+        <ul>
+            <li>Firefox KDE</li>
+            <li>Pidgin</li>
+            <li>JDownloader</li> 
+            <li>Qbittorrent </li>
+        </ul>
+        <strong>Gráficos:</strong>
+        <ul>
+            <li>Gwenview</li>
+            <li>Kamoso</li>
+            <li>Skanlite</li>
+            <li>Ksnapshot</li>
+        </ul>
+        <strong>Multimídia:</strong>
+        <ul>
+            <li>K3B</li>
+            <li>SMplayer</li> 
+            <li>Clementine</li>
+            <li>YouTube Browser (SMplayer YouTube Browser)</li>
+            <li>Kmix</li>
+        </ul>
+        <strong>Sistema:</strong>
+        <ul>        
+            <li>Central de Programas do Ubuntu</li>
+            <li>Konsole (Terminal)</li>
+            <li>Dolphin</li>
+            <li>Jockey driver manager (Instalador de Drivers)</li>
+            <li>OpenJDK</li>
+            <li>Synaptic</li>
+            <li>Centro de Controle Big</li>
+        </ul>
+        <strong>Acessórios:</strong>
+        <ul>
+            <li>Kate</li>
+            <li>Kcalc</li>
+            <li>Klipper</li>
+            <li>Ark</li>
+            <li>Knotes</li>
+            <li>Kmousetools</li>
+        </ul>
+        <strong>Outros:</strong>
+        <ul>
+            <li>Ubuntu Restricted Extras</li>
+            <li>Homerun Launcher Kicker</li>
+            <li>prelink-fix</li>
+            <li>ImageMagick</li>
+        </ul>
+    </p>
+</div>
